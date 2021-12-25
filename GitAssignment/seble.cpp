d41008ac787fe1493889539ecd5737ea85080013
Mohammed Bilacha Kache <96645854+MohammedBilacha@users.noreply.github.com>
2021-12-25 11:26:44 +0300

    Update seble.cpp

--- GitAssignment/seble.cpp
+++ GitAssignment/seble.cpp
@@ -8,6 +8,7 @@ array[index1][index2]=index1 + index2;
 for(index1=0;index1<4;index1++) { 
 for(index2=0;index2<4;index2++) 
 cout << array[index1][index2] << " "; 
+  cout<<"Mohammed Bilacha";
 cout << endl; 
 
 
